1eba4e8f72427edf007aa3a00779afbbfef41c43
thang2k6adu <thang2k6adu@gmail.com>
2025-10-25 16:58:28 +0700

    [CONFIG] setup toast

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,22 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import reportWebVitals from './reportWebVitals';
-import 'react-multi-carousel/lib/styles.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import ShopApplicationWrapper from './pages/ShopApplicationWrapper';
+import "react-multi-carousel/lib/styles.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import ShopApplicationWrapper from "./pages/ShopApplicationWrapper";
+import { Toaster } from "react-hot-toast";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <RouterProvider router={router}>
-      
-        <ShopApplicationWrapper />
-      
+      <ShopApplicationWrapper />
+      <Toaster />
     </RouterProvider>
-    </Provider>
+  </Provider>
 );
-
 
 // reportWebVitals();
