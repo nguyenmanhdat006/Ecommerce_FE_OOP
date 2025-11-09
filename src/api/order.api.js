014d9c8261ff18f3eb7c160ffdb5ee489340bab3
phamhuy <phamhuy1032006@gmail.com>
2025-11-10 00:24:32 +0700

    init orderAPI

--- src/api/order.api.js
+++ src/api/order.api.js
@@ -1,17 +1,17 @@
 import axiosClient from "./axiosClient";
 
 export const orderAPI = {
-  // POST /api/orders
+  // POST 
   create: (data) => axiosClient.post("/api/orders", data),
 
   // GET
   getById: (id) => axiosClient.get(`/api/orders/${id}`),
   getAll: () => axiosClient.get("/api/orders"),
 
-  // DELETE - xóa đơn hàng
+  // DELETE 
   delete: (orderId) => axiosClient.delete(`/api/orders/${orderId}`),
 
-  // PUT - cập nhật trạng thái đơn hàng
+  // PUT 
   updateStatus: (orderId, newStatus, changedBy) =>
     axiosClient.put(
       `/api/orders/${orderId}/status${
@@ -20,6 +20,6 @@ export const orderAPI = {
       { status: newStatus }
     ),
 
-  // PUT - cập nhật đơn hàng
+  // PUT 
   update: (orderId, data) => axiosClient.put(`/api/orders/${orderId}`, data),
 };
